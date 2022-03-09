25c29d6a9054cb990c195b58cf4ddc223c3deace
Dima Yeroshenko <dyeroshenko@google.com>
2022-03-09 12:53:12 +0000

    initial commit with scraping logic

---
+++ app/config.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+
+
+class TheGuardianConfig:
+    URL = 'https://www.theguardian.com/world'
+    NAME = 'the guardian'
+
+class BBCConfig:
+    URL = 'https://www.bbc.com/news'
+    NAME = 'bbc'
+
+class TimeConfig:
+    URL = 'https://time.com/'
+    NAME = 'time'
