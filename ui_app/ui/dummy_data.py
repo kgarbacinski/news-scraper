e475165522765c330b8d5764cfd44fd0fd5d5c10
Dima Yeroshenko <dyeroshenko@google.com>
2022-03-11 20:21:33 +0000

    added sorting to output dict with data

--- ui_app/ui/dummy_data.py
+++ ui_app/ui/dummy_data.py
@@ -1,124 +1,154 @@
 DUMMY_DATA = {
-  "bbc": [
-    [
-      "Foreigners go to join Ukraine fight", 
-      "https://www.bbc.com/news/world-europe-60696603"
-    ], 
-    [
-      "Putin seeks foreign volunteers to fight in Ukraine", 
-      "https://www.bbc.com/news/world-europe-60705486"
-    ], 
-    [
-      "How 'Saint Javelin' raised over $1m for Ukraine", 
-      "https://www.bbc.com/news/world-us-canada-60700906"
-    ], 
-    [
-      "Lorry drivers' 1,300-mile trip with Ukraine aid", 
-      "https://www.bbc.com/news/uk-england-nottinghamshire-60680951"
-    ], 
-    [
-      "UK sanctions Russian MPs over Ukraine invasion", 
-      "https://www.bbc.com/news/uk-politics-60708230"
+    "bbc": [
+        [
+            "Lorry drivers' 1,300-mile trip with Ukraine aid",
+            "https://www.bbc.com/news/uk-england-nottinghamshire-60680951"
+        ],
+        [
+            "What if dirty bombs are used in Ukraine? And other questions",
+            "https://www.bbc.com/news/world-60711951"
+        ],
+        [
+            "UK sanctions Russian MPs over Ukraine invasion",
+            "https://www.bbc.com/news/uk-politics-60708230"
+        ],
+        [
+            "How 'Saint Javelin' raised over $1m for Ukraine",
+            "https://www.bbc.com/news/world-us-canada-60700906"
+        ],
+        [
+            "Foreigners go to join Ukraine fight",
+            "https://www.bbc.com/news/world-europe-60696603"
+        ],
+        [
+            "Putin seeks foreign volunteers to fight in Ukraine",
+            "https://www.bbc.com/news/world-europe-60705486"
+        ],
+        [
+            "Mystery drone from Ukraine war crashes in Croatia",
+            "https://www.bbc.com/news/world-europe-60709952"
+        ],
+        [
+            "US warns Russia may be planning Ukraine escalation",
+            "https://www.bbc.com/news/live/world-europe-60685883"
+        ]
+    ],
+    "new york times": [
+        [
+            "Lviv, UkraineBrendan Hoffman for The New York TimesLviv, UkraineBrendan Hoffman for The New York TimesLviv, UkraineBrendan Hoffman for The New York TimesBrovary, UkraineLynsey Addario for The New York TimesBrovary, UkraineLynsey Addario for The New York TimesDnipro, UkraineEmre Caylak/Agence France-Presse \u2014 Getty ImagesKyiv, UkraineSergei Supinsky/Agence France-Presse \u2014 Getty ImagesLviv, UkraineBrendan Hoffman for The New York TimesZahony, HungaryMauricio Lima for The New York TimesMedyka, PolandMaciek Nabrdalik for The New York Times",
+            "https://www.nytimes.com/live/2022/03/11/world/ukraine-russia-news"
+        ],
+        [
+            "Russia-Ukraine War1m ago",
+            "https://www.nytimes.com/live/2022/03/11/world/ukraine-russia-news"
+        ],
+        [
+            "Ukraine\u2019s vast array of historic buildings, artworks and public squares are being reduced to rubble.",
+            "https://www.nytimes.com/2022/03/11/travel/ukraine-architecture-war.html"
+        ],
+        [
+            "So Much Suffering in the Ukraine War",
+            "https://www.nytimes.com/2022/03/11/opinion/letters/russia-ukraine-war.html"
+        ],
+        [
+            "We fact-checked the unfounded theory amplified by right-wing voices about U.S.-funded bioweapons labs in Ukraine.",
+            "https://www.nytimes.com/2022/03/11/us/politics/us-bioweapons-ukraine-misinformation.html"
+        ],
+        [
+            "Congress Clears $1.5 Trillion Spending Bill, Including Ukraine Aid",
+            "https://www.nytimes.com/2022/03/10/us/politics/senate-spending-bill-approved.html"
+        ],
+        [
+            "LiveU.N. Says 564 Civilians Killed So Far; Full Toll Is Likely to Be HigherCalling on Congress to restrict trade with Russia, President Biden said he would also sign an order that would ban imports such as seafood, vodka and other goods.Russian strikes targeted locations far from the front lines in Ukraine and pummeled hard-hit places, where conditions were increasingly dire. Here\u2019s the latest.",
+            "https://www.nytimes.com/live/2022/03/11/world/ukraine-russia-news"
+        ],
+        [
+            "Europe\u2019s railways are on the war footing, taking fighters to Ukraine and bringing back refugees.",
+            "https://www.nytimes.com/2022/03/11/world/europe/ukraine-fighters-refugees-trains.html"
+        ],
+        [
+            "Turkish-made drones have become a rallying symbol for Ukraine, through songs and social media posts.",
+            "https://www.nytimes.com/2022/03/11/us/politics/ukraine-military-drones-russia.html"
+        ]
+    ],
+    "the guardian": [
+        [
+            "US and allies set to revoke normal trade relations with Russia over Ukraine war, says Biden \u2013 follow live",
+            "https://www.theguardian.com/us-news/live/2022/mar/11/joe-biden-suspension-normal-trade-russia-ukraine-us-politics-latest"
+        ],
+        [
+            "Russia widens attack with airstrikes on western Ukraine cities",
+            "https://www.theguardian.com/world/2022/mar/11/russia-widens-attack-with-airstrikes-on-western-ukraine-cities"
+        ],
+        [
+            "Live  US and allies set to revoke normal trade relations with Russia over Ukraine war, says Biden \u2013 follow live",
+            "https://www.theguardian.com/us-news/live/2022/mar/11/joe-biden-suspension-normal-trade-russia-ukraine-us-politics-latest"
+        ],
+        [
+            "West\u2019s weapons are seeing action in Ukraine \u2013 but it is unlikely to be enough",
+            "https://www.theguardian.com/world/2022/mar/11/west-weapons-seeing-action-ukraine-but-unlikely-enough-russia-war"
+        ],
+        [
+            "Live  Russia-Ukraine war latest news: Russia could use chemical and biological weapons, US warns, as sanctions against Moscow are tightened",
+            "https://www.theguardian.com/world/live/2022/mar/11/russia-ukraine-war-latest-news-we-are-dealing-with-a-terrorist-state-says-zelenskiy-biden-plans-fresh-russian-trade-crackdown-live"
+        ],
+        [
+            "Ukraine",
+            "https://www.theguardian.com/world/ukraine"
+        ],
+        [
+            "\u2018We need bread\u2019: fears in Middle East as Ukraine war hits wheat imports",
+            "https://www.theguardian.com/global-development/2022/mar/07/we-need-bread-fears-in-middle-east-as-ukraine-russia-war-hits-wheat-imports"
+        ],
+        [
+            "Ukraine woman who escaped Mariupol maternity ward gives birth",
+            "https://www.theguardian.com/world/2022/mar/11/ukraine-woman-who-escaped-mariupol-maternity-ward-gives-birth"
+        ],
+        [
+            "Trump thought US troops were in Ukraine in 2017, ex-ambassador says in book",
+            "https://www.theguardian.com/books/2022/mar/11/donald-trump-ukraine-crimea-russia-poroshenko-book"
+        ],
+        [
+            "Narendra Modi walks diplomacy tightrope with Vladimir Putin on Ukraine",
+            "https://www.theguardian.com/world/2022/mar/10/narendra-modi-walks-diplomacy-tightrope-with-vladimir-putin-on-ukraine"
+        ],
+        [
+            "Russia\u2019s \u2018warning\u2019 of Ukraine\u2019s biological weapons sounds just like Syria",
+            "https://www.theguardian.com/world/2022/mar/09/russias-warning-of-ukraines-biological-weapons-sounds-just-like-syria"
+        ],
+        [
+            "\u2018My plan is there is no plan\u2019: the foreign fighters flocking to Ukraine",
+            "https://www.theguardian.com/world/2022/mar/11/ukraine-russia-war-foreign-fighters-volunteers"
+        ],
+        [
+            "Russia-Ukraine war latest news: Russia could use chemical and biological weapons, US warns, as sanctions against Moscow are tightened",
+            "https://www.theguardian.com/world/live/2022/mar/11/russia-ukraine-war-latest-news-we-are-dealing-with-a-terrorist-state-says-zelenskiy-biden-plans-fresh-russian-trade-crackdown-live"
+        ]
+    ],
+    "time": [
+        [
+            "Could Protests Against the Ukraine War Be Putin's Undoing?",
+            "https://time.com//6154240/russia-protests-war-ukraine-putin/"
+        ],
+        [
+            "How the Ukraine-Russia Conflict Will Raise the Price of Snack Foods",
+            "https://time.com//6155095/sunflower-oil-russia-ukraine/"
+        ],
+        [
+            "The Russian Grand Strategy Guiding the Invasion of Ukraine",
+            "https://time.com//6155129/russian-strategy-guiding-invasion-ukraine/"
+        ],
+        [
+            "The Foreign Volunteers Risking Their Lives in Ukraine",
+            "https://time.com//6155670/foreign-fighters-ukraine-europe/"
+        ],
+        [
+            "The Race to Secure Ukraine's Nuclear Plants",
+            "https://time.com//6155117/inside-the-urgent-race-to-secure-ukraines-nuclear-plants/"
+        ],
+        [
+            "Only the Threat of Force Will Stop Putin Going Beyond Ukraine",
+            "https://time.com//6155863/putin-threat-baltic-states/"
+        ]
     ]
-  ], 
-  "the guardian": [
-    [
-      "\u2018My plan is there is no plan\u2019: the foreign fighters flocking to Ukraine", 
-      "https://www.theguardian.com/world/2022/mar/11/ukraine-russia-war-foreign-fighters-volunteers"
-    ], 
-    [
-      "Russia widens attack with airstrikes on western Ukraine cities", 
-      "https://www.theguardian.com/world/2022/mar/11/russia-widens-attack-with-airstrikes-on-western-ukraine-cities"
-    ], 
-    [
-      "Ukraine military drone crashes into Croatian capital Zagreb", 
-      "https://www.theguardian.com/world/2022/mar/11/ukraine-military-drone-crashes-into-croatian-capital-zagreb"
-    ], 
-    [
-      "Ukraine", 
-      "https://www.theguardian.com/world/ukraine"
-    ], 
-    [
-      "Russia\u2019s \u2018warning\u2019 of Ukraine\u2019s biological weapons sounds just like Syria", 
-      "https://www.theguardian.com/world/2022/mar/09/russias-warning-of-ukraines-biological-weapons-sounds-just-like-syria"
-    ], 
-    [
-      "Ukraine\u2019s Zelenskiy calls Russia \u2018terrorist state\u2019 over aid shelling as forces close in on Kyiv", 
-      "https://www.theguardian.com/world/2022/mar/11/ukraines-zelenskiy-calls-russia-terrorist-state-over-aid-shelling-as-forces-close-in-on-kyiv"
-    ], 
-    [
-      "Russia-Ukraine war latest news: Ukraine at \u2018strategic turning point\u2019, claims Zelenskiy, as Russian airstrikes hit three cities", 
-      "https://www.theguardian.com/world/live/2022/mar/11/russia-ukraine-war-latest-news-we-are-dealing-with-a-terrorist-state-says-zelenskiy-biden-plans-fresh-russian-trade-crackdown-live"
-    ], 
-    [
-      "Narendra Modi walks diplomacy tightrope with Vladimir Putin on Ukraine", 
-      "https://www.theguardian.com/world/2022/mar/10/narendra-modi-walks-diplomacy-tightrope-with-vladimir-putin-on-ukraine"
-    ], 
-    [
-      "Russia-Ukraine war in numbers: casualties, refugees and aid", 
-      "https://www.theguardian.com/world/2022/mar/11/russia-ukraine-war-numbers-casualties-refugees-aid"
-    ], 
-    [
-      "Live  UK politics live: UK will support Ukraine with weapons and diplomacy in face of \u2018barbaric war\u2019, says Truss", 
-      "https://www.theguardian.com/politics/live/2022/mar/11/uk-politics-live-government-plans-sponsored-ukrainian-refugees-stormont-apology-boris-johnson-russia-ukraine-latest-updates"
-    ], 
-    [
-      "UK politics live: UK will support Ukraine with weapons and diplomacy in face of \u2018barbaric war\u2019, says Truss", 
-      "https://www.theguardian.com/politics/live/2022/mar/11/uk-politics-live-government-plans-sponsored-ukrainian-refugees-stormont-apology-boris-johnson-russia-ukraine-latest-updates"
-    ], 
-    [
-      "\u2018We need bread\u2019: fears in Middle East as Ukraine war hits wheat imports", 
-      "https://www.theguardian.com/global-development/2022/mar/07/we-need-bread-fears-in-middle-east-as-ukraine-russia-war-hits-wheat-imports"
-    ], 
-    [
-      "Putin approves Russian use of Middle East fighters against Ukraine", 
-      "https://www.theguardian.com/world/2022/mar/11/putin-approves-russian-use-of-middle-east-fighters-against-ukraine"
-    ], 
-    [
-      "Live  Russia-Ukraine war latest news: Ukraine at \u2018strategic turning point\u2019, claims Zelenskiy, as Russian airstrikes hit three cities", 
-      "https://www.theguardian.com/world/live/2022/mar/11/russia-ukraine-war-latest-news-we-are-dealing-with-a-terrorist-state-says-zelenskiy-biden-plans-fresh-russian-trade-crackdown-live"
-    ]
-  ], 
-  "time": [
-    [
-      "The Foreign Volunteers Risking Their Lives in Ukraine", 
-      "https://time.com//6155670/foreign-fighters-ukraine-europe/"
-    ], 
-    [
-      "The Russian Grand Strategy Guiding the Invasion of Ukraine", 
-      "https://time.com//6155129/russian-strategy-guiding-invasion-ukraine/"
-    ], 
-    [
-      "Bellingcat's Eliot Higgins on the Ukraine Information War", 
-      "https://time.com//6155869/bellingcat-eliot-higgins-ukraine-open-source-intelligence/"
-    ], 
-    [
-      "Germany Rolls Back Environmental Rules To Boost Domestic Crops Threatened by Russia's War in Ukraine", 
-      "https://time.com//6156901/germany-environmental-rules-crops-russia-war-ukraine/"
-    ], 
-    [
-      "How the Ukraine-Russia Conflict Will Raise the Price of Snack Foods", 
-      "https://time.com//6155095/sunflower-oil-russia-ukraine/"
-    ], 
-    [
-      "Could Protests Against the Ukraine War Be Putin's Undoing?", 
-      "https://time.com//6154240/russia-protests-war-ukraine-putin/"
-    ], 
-    [
-      "The Race to Secure Ukraine's Nuclear Plants", 
-      "https://time.com//6155117/inside-the-urgent-race-to-secure-ukraines-nuclear-plants/"
-    ], 
-    [
-      "Ukraine Crisis Spurs Russophobia Around the World", 
-      "https://time.com//6156582/ukraine-anti-russian-hate/"
-    ], 
-    [
-      "Only the Threat of Force Will Stop Putin Going Beyond Ukraine", 
-      "https://time.com//6155863/putin-threat-baltic-states/"
-    ], 
-    [
-      "Putin\u2019s War in Ukraine Will Make New Cars More Expensive", 
-      "https://time.com//6156679/car-prices-ukraine-russia/"
-    ]
-  ]
 }
