5ee1700e1646fd344c0590310ed0af50b229bae1
Dima Yeroshenko <dyeroshenko@google.com>
2022-03-30 23:05:02 +0100

    Fixed Django host to run on 0.0.0.0

--- ui_app/ui_app/settings.py
+++ ui_app/ui_app/settings.py
@@ -5,7 +5,7 @@ from django.core.management.utils import get_random_secret_key
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = get_random_secret_key()
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 
 
 # Application definition
