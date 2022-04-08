4c9e396e117d473ffc48e4bd6302e9044d3d4e9d
Dima Yeroshenko <dyeroshenko@google.com>
2022-04-08 15:25:25 +0100

    Added data fetching from bsckend and basic content render

--- services/ui_app/ui/static/ui/js/main.js
+++ services/ui_app/ui/static/ui/js/main.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const csrfToken = generateCookie('csrftoken');
-    const contentDiv = document.getElementById('content');
-    const keyword = 'war';
+    const contentDiv = document.getElementById('scraped_content');
+    const form = document.getElementById('form');
+    const input = document.getElementById('keyword')
 
 
     async function insertTask(keyword) {
@@ -21,7 +22,6 @@ document.addEventListener("DOMContentLoaded", function () {
         return data['task_id'];
     }
 
-
     async function fetchData(url) {
         let response = await fetch(url, {
             method: 'GET',
@@ -35,7 +35,6 @@ document.addEventListener("DOMContentLoaded", function () {
         return result
     }
 
-
     function getContent(task_id) {
         setTimeout(async function () {
             let url = `${window.location.protocol}//${window.location.hostname}:8004/tasks/${task_id}`
@@ -46,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (task_status == 'SUCCESS') {
                 console.log('Content is ready!');
                 console.log(content);
-                contentDiv.textContent += content.keyword
+                renderContent(content);
                 return
             } else {
                 console.log('Checking for content...');
@@ -55,8 +54,47 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 100)
     }
 
+    function renderContent(data) {
+        let content = data.articles;
+        let query = data.keyword;
+        
+        for (key in content) {
+            let articles_array = content[key]
 
-    async function run(keyword) {
+            for (i = 0; i < articles_array.length; i++) {
+                let title = articles_array[i][0];
+                let link = articles_array[i][1];
+
+                contentDiv.innerHTML += `
+                <div class='source_data'>
+                    <span class='source_header'>${key.toUpperCase()} | ${articles_array.length}</span>
+                    <ul>
+                        <li><span class='article_title'>${title}</span>: <a href='${link}' target='_blank'>more</a></li>
+                    </ul>
+                </div>
+                `
+            }
+        }
+    }
+
+    function removeContent() {
+        contentDiv.innerHTML = '';
+    }
+
+    function runQuery() {
+        form.addEventListener('submit', function (e) {
+            e.preventDefault();
+            removeContent();
+
+            const keyword = input.value;
+            console.log(`input: ${keyword}`);
+
+            startScraping(keyword);
+        })
+        
+    }
+
+    async function startScraping(keyword) {
         let task_id = await insertTask(keyword);
         getContent(task_id);
     }
@@ -77,5 +115,5 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
 
-    run(keyword)
+    runQuery();
 });
--- services/ui_app/ui/views.py
+++ services/ui_app/ui/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
-from .dummy_data import DUMMY_DATA
-
 def main_view(request):
-    return render(request, 'ui/main.html', {'title': 'News Scraper', 'articles': DUMMY_DATA})
+    return render(request, 'ui/main.html', {'title': 'News Scraper'})
