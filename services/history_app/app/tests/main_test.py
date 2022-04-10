afd9c4f40e7062b9967f36428f40345ce9e80e21
Dima Yeroshenko <dyeroshenko@google.com>
2022-04-11 00:18:00 +0100

    Added dummy db config to test app in CI

--- services/history_app/app/tests/main_test.py
+++ services/history_app/app/tests/main_test.py
@@ -1,8 +1,22 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
 from app.endpoints import app
+from db.database import Base
 
 client = TestClient(app)
 
+DUMMY_DB = "sqlite:///./test.db"
+
+engine = create_engine(
+    DUMMY_DB, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
 
 def test_read_main():
     response = client.get("/")
