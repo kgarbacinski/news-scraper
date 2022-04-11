a31b6420ebe8150e2dfa203e4f193f570664cd8f
Dima Yeroshenko <dyeroshenko@google.com>
2022-04-11 15:50:14 +0100

    Added tests to Django ui_app

--- services/ui_app/history/tests.py
+++ services/ui_app/history/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class TestHistoryView(TestCase):
+    URL = "/"
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_should_return_200_when_app_is_running(self) -> None:
+        response = self.client.get(TestHistoryView.URL)
+
+        self.assertEqual(response.status_code, 200)
--- services/ui_app/ui/tests.py
+++ services/ui_app/ui/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class TestHomeView(TestCase):
+    URL = "/"
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_should_return_200_when_app_is_running(self) -> None:
+        response = self.client.get(TestHomeView.URL)
+
+        self.assertEqual(response.status_code, 200)
